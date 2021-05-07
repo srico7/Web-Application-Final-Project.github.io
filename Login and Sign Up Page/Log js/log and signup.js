3f7a89f4086a4d5af80275293206c70167d86f1d
Vikum Dineth Amarananda <72123359+dinethviku@users.noreply.github.com>
2021-05-08 01:58:24 +0530

    Update

--- Login and Sign Up Page/Log js/log and signup.js
+++ Login and Sign Up Page/Log js/log and signup.js
@@ -1,6 +1,6 @@
 const sign_in_button = document.querySelector("#sign-in-button");
 const sign_up_button = document.querySelector("#sign-up-button");
-const containers = document.querySelector(".containers");
+const conteiners = document.querySelector(".containers");
 
 sign_up_button.addEventListener('click', () => {
     containers.classList.add("sign-up-mode");
