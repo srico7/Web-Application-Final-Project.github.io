39b3586b6e25e92152cf37bd5eee81d63199b1d7
Vikum Dineth Amarananda <72123359+dinethviku@users.noreply.github.com>
2021-05-08 01:43:22 +0530

    Js
    set js

---
+++ Login and Sign Up Page/Log js/log and signup.js
@@ -0,0 +1,11 @@
+const sign_in_button = document.querySelector("#sign-in-button");
+const sign_up_button = document.querySelector("#sign-up-button");
+const containers = document.querySelector(".containers");
+
+sign_up_button.addEventListener('click', () => {
+    containers.classList.add("sign-up-mode");
+});
+
+sign_in_button.addEventListener('click', () => {
+    containers.classList.remove("sign-up-mode");
+});
