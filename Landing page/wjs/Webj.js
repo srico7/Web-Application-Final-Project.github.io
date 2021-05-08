a52c54d1b21a23cf92967aa5c716ba097ef28a03
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2021-05-09 01:15:37 +0530

    Hamburger Menu
    Animation

--- Landing page/wjs/Webj.js
+++ Landing page/wjs/Webj.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+const hambtn = document.querySelector('#hambtn');
+const header = document.querySelector('.header');
+hambtn.addEventListener('click', function(){
+    console.log('click hambtn');
+
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+    }
+    else{
+        header.classList.add('open');
+    }
+});
