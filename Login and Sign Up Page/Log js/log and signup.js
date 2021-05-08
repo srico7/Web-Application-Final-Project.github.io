dd39d0b778f3cd7df1048a351ea51459c5a6e0cf
Vikum Dineth Amarananda <72123359+dinethviku@users.noreply.github.com>
2021-05-08 15:06:55 +0530

    Animation Create
    start animate login page

--- Login and Sign Up Page/Log js/log and signup.js
+++ Login and Sign Up Page/Log js/log and signup.js
@@ -1,11 +1,13 @@
 const sign_in_button = document.querySelector("#sign-in-button");
 const sign_up_button = document.querySelector("#sign-up-button");
-const conteiners = document.querySelector(".containers");
+const conteiners = document.querySelector(".conteiners");
 
-sign_up_button.addEventListener('click', () => {
-    containers.classList.add("sign-up-mode");
+sign_up_button.addEventListener('click', function () {
+    conteiners.classList.add("sign-up-mode");
 });
 
-sign_in_button.addEventListener('click', () => {
-    containers.classList.remove("sign-up-mode");
+sign_in_button.addEventListener('click',function () {
+    conteiners.classList.remove("sign-up-mode");
 });
+
+document.write('hello');
