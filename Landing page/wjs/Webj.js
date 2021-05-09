19689bd79ed0b21850367f9decf01e8af332c350
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2021-05-10 01:04:20 +0530

    nav done
    Having problems with drop hamburger menu

--- Landing page/wjs/Webj.js
+++ Landing page/wjs/Webj.js
@@ -1,12 +1,21 @@
 const hambtn = document.querySelector('#hambtn');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 hambtn.addEventListener('click', function(){
     console.log('click hambtn');
 
     if(header.classList.contains('open')){
         header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
+    
     else{
         header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+          });
     }
 });
