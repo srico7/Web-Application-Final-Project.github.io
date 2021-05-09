e9f4b7077f30622af37bc589db025e93e14722f0
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2021-05-10 02:13:25 +0530

    Nav bar fully fixed

--- Landing page/wjs/Webj.js
+++ Landing page/wjs/Webj.js
@@ -1,21 +1,28 @@
 const hambtn = document.querySelector('#hambtn');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
+
 hambtn.addEventListener('click', function(){
     console.log('click hambtn');
 
-    if(header.classList.contains('open')){
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-    }
-    
-    else{
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        body.classList.remove('scroll');
+        header.classList.remove('open');    
+        fadeElems.forEach(function(element){
+          element.classList.remove('fade-in');
+          element.classList.add('fade-out');
+        });
+        
+      }
+      else { // Open Hamburger Menu
+        body.classList.add('scroll');
         header.classList.add('open');
         fadeElems.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-          });
-    }
-});
+          element.classList.remove('fade-out');
+          element.classList.add('fade-in');
+        });
+    
+      }  
+    }); 
